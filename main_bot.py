c30524efd805cd944b1314c34500a0a02e52a36d
Abel Wendmu <abelwendmu124@gmail.com>
2024-05-21 21:57:07 +0300

    implimentation  registeration

--- main_bot.py
+++ main_bot.py
@@ -1,3 +1,6 @@
+from request_util import Request_to_Django
+
+
 from telegram import (
     Update,
     InlineKeyboardButton,
@@ -25,7 +28,7 @@ replay_keyboard = [
 ]
 markup = InlineKeyboardMarkup(replay_keyboard)
 choose_client_org = 0
-register_org = 1
+register_client = 1
 (
     to_full_name,
     to_date_of_birth,
@@ -38,7 +41,15 @@ register_org = 1
     to_linkedin,
     to_save,
     to_done,
-) = range(2, 12)
+    register_org,
+    to_org_name,
+    to_type,
+    to_email_org,
+    to_phone_number_org,
+    to_description,
+    to_website,
+    to_address,
+) = range(2, 21)
 client_profile = {}
 org_profile = {}
 
@@ -47,17 +58,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     await update.message.reply_text("hey abel", reply_markup=markup)
     return choose_client_org
 
-#choose client or organization
+# choose client or organization
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
-    if query.data == "client":
-        if context._user_id in ["response"]:
-            await query.message.reply_text("You are in what you want to do")
-            return register_org
-        else:
-            await query.message.reply_text("You are not regestered")
-            return register_org
-    else:
+    if query.data == "org":
         if context._user_id in ["response"]:
             await query.message.reply_text("You are in what you want to do")
             return register_org
@@ -71,8 +75,101 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 "You are not regestered", reply_markup=markup
             )
             return register_org
+    else:
+        if context._user_id in ["response"]:
+            await query.message.reply_text("You are in what you want to do")
+            return register_client
+        else:
+            replay_keyboard = [
+                [InlineKeyboardButton("REGISTER", callback_data="register")],
+                [InlineKeyboardButton("EXIT", callback_data="exit")],
+            ]
+            markup = InlineKeyboardMarkup(replay_keyboard)
+            await query.message.reply_text(
+                "You are not regestered", reply_markup=markup
+            )
+            return register_client
 
-#register client
+
+# register organization
+async def register_org(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    if query.data == "register":
+        await context.bot.send_message(
+            chat_id=context._user_id,
+            text="write your organization name",
+        )
+        return to_org_name
+    else:
+        await context.bot.send_message(chat_id=context._user_id, text="Bye")
+        return ConversationHandler.END
+
+
+# name of the organization
+async def org_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    org_profile["telegramId"] = context._user_id
+    org_profile["telegramUsername"] = context._user_id
+    org_profile["name"] = update.message.text
+    await context.bot.send_message(chat_id=context._user_id, text="type")
+    return to_type
+
+
+# type of the organization
+async def org_type(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    org_profile["type"] = update.message.text
+    await context.bot.send_message(
+        chat_id=context._user_id, text="please send your email"
+    )
+    return to_email_org
+
+
+# register email
+async def org_email(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    org_profile["email"] = update.message.text
+    await context.bot.send_message(
+        chat_id=context._user_id, text="please send your phone number"
+    )
+    return to_phone_number_org
+
+
+# register phone number
+async def org_phone_number(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    org_profile["phoneNumber"] = update.message.text
+    await context.bot.send_message(
+        chat_id=context._user_id, text="tell us about your organization"
+    )
+    return to_description
+
+
+# about the organization
+async def org_description(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    org_profile["description"] = update.message.text
+    await context.bot.send_message(chat_id=context._user_id, text="send your website")
+    return to_website
+
+
+# register website
+async def org_website(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    org_profile["website"] = update.message.text
+    await context.bot.send_message(
+        chat_id=context._user_id,
+        text="where we can find you for example 'addis ababa, bole, 01'",
+    )
+    return to_address
+
+
+# register address
+async def org_address(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    org_profile["city"], org_profile["subCity"], org_profile["kebele"] = [
+        k.strip() for k in update.message.text.split(",")
+    ]
+    await context.bot.send_message(
+        chat_id=context._user_id, text="do you agree tell me"
+    )
+    return to_save
+
+
+# register client
 async def register(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     if query.data == "register":
@@ -85,25 +182,27 @@ async def register(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await context.bot.send_message(chat_id=context._user_id, text="Bye")
         return ConversationHandler.END
 
-#name of the client 
+# name of the client
 async def full_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    client_profile["telegramId"] = context._user_id
-    client_profile["telegramUsername"] = context._user_name
+    client_profile["telegramId"] = str(context._user_id)
+    client_profile["telegramUsername"] = str(context._user_id) + "@telegram"
     client_profile["firstName"], client_profile["lastName"] = update.message.text.split(
         " "
     )
-    await context.bot.send_message(chat_id=context._user_id, text="date of birth")
+    await context.bot.send_message(
+        chat_id=context._user_id, text="date of birth '2001-11-05'"
+    )
     return to_date_of_birth
 
-#register date of birth
+# register date of birth
 async def date_of_birth(update: Update, context: ContextTypes.DEFAULT_TYPE):
     client_profile["dateOfBirth"] = update.message.text
     await context.bot.send_message(
-        chat_id=context._user_id, text="please send your email"
+        chat_id=context._user_id, text="please send your email like "
     )
     return to_email
 
-#register email
+# register email
 async def email(update: Update, context: ContextTypes.DEFAULT_TYPE):
     client_profile["email"] = update.message.text
     await context.bot.send_message(
@@ -111,23 +210,23 @@ async def email(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
     return to_phone_number
 
-#register phone number
+# register phone number
 async def phone_number(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    client_profile["phone_number"] = update.message.text
+    client_profile["phoneNumber"] = update.message.text
     await context.bot.send_message(
         chat_id=context._user_id, text="what is your highest education level"
     )
     return to_highest_education_level
 
-#register highest education level
+# register highest education level
 async def highest_education_level(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    client_profile["highest_education_level"] = update.message.text
+    client_profile["highestEducationLevel"] = update.message.text
     await context.bot.send_message(
         chat_id=context._user_id, text="link of your protfolio"
     )
     return to_portfolio
 
-#register portfolio
+# register portfolio
 async def portfolio(update: Update, context: ContextTypes.DEFAULT_TYPE):
     client_profile["portfolio"] = update.message.text
     await context.bot.send_message(
@@ -135,7 +234,7 @@ async def portfolio(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
     return to_cv
 
-#register cv
+# register cv
 async def cv(update: Update, context: ContextTypes.DEFAULT_TYPE):
     client_profile["cv"] = update.message.text
     await context.bot.send_message(
@@ -143,7 +242,7 @@ async def cv(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
     return to_github
 
-#register github
+# register github
 async def github(update: Update, context: ContextTypes.DEFAULT_TYPE):
     client_profile["github"] = update.message.text
     await context.bot.send_message(
@@ -151,13 +250,15 @@ async def github(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
     return to_linkedin
 
-#register linkedin
+# register linkedin
 async def linkedin(update: Update, context: ContextTypes.DEFAULT_TYPE):
     client_profile["linkedin"] = update.message.text
-    await context.bot.send_message(chat_id=context._user_id, text="Bye")
+    await context.bot.send_message(
+        chat_id=context._user_id, text="do you agree tell me"
+    )
     return to_save
 
-#save the data
+# save the data
 async def done(update: Update, context: ContextTypes.DEFAULT_TYPE):
     replay_keyboard = [
         [InlineKeyboardButton("YES", callback_data="save")],
@@ -166,26 +267,40 @@ async def done(update: Update, context: ContextTypes.DEFAULT_TYPE):
     markup = InlineKeyboardMarkup(replay_keyboard)
     await context.bot.send_message(
         chat_id=context._user_id,
-        text="do your want to save your data",
+        text="are you sure?",
         reply_markup=markup,
     )
     return to_done
 
-#save the data
+# save the data
 async def save(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
+
     if query.data == "save":
-        await context.bot.send_message(chat_id=context._user_id, text="Data saved")
+        profile = {"telegramId": str(context._user_id)}
+        for i in ["highestEducationLevel", "portfolio", "cv", "github", "linkedin"]:
+            if i in client_profile:
+                profile[i] = client_profile[i]
+                del client_profile[i]
+        request = Request_to_Django(
+            endpoint="http://localhost:5000/api/v1/employee/register"
+        )
+        res = request.post_request(client_profile)
+        request = Request_to_Django(
+            endpoint="http://localhost:5000/api/v1/profile/create"
+        )
+        res_pro = request.post_request(profile)
+        await context.bot.send_message(chat_id=context._user_id, text=res.data)
+
+        return ConversationHandler.END
     else:
         await context.bot.send_message(chat_id=context._user_id, text="Bye")
         return ConversationHandler.END
 
-#exit the conversation
+# exit the conversation
 async def exit(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=context._user_id, text="Bye")
     return ConversationHandler.END
-
-
 
 
 conversation_one = ConversationHandler(
@@ -196,7 +311,7 @@ conversation_one = ConversationHandler(
                 button,
             )
         ],
-        register_org: [
+        register_client: [
             CallbackQueryHandler(
                 register,
             ),
@@ -224,6 +339,21 @@ conversation_one = ConversationHandler(
                 exit,
             ),
         ],
+        register_org: [
+            CallbackQueryHandler(
+                register_org,
+            ),
+            CallbackQueryHandler(
+                exit,
+            ),
+        ],
+        to_org_name: [MessageHandler(~filters.COMMAND, org_name)],
+        to_type: [MessageHandler(~filters.COMMAND, org_type)],
+        to_email_org: [MessageHandler(~filters.COMMAND, org_email)],
+        to_description: [MessageHandler(~filters.COMMAND, org_description)],
+        to_phone_number_org: [MessageHandler(~filters.COMMAND, org_phone_number)],
+        to_website: [MessageHandler(~filters.COMMAND, org_website)],
+        to_address: [MessageHandler(~filters.COMMAND, org_address)],
     },
     fallbacks=[CommandHandler("cancel", start)],
 )
